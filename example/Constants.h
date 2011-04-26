d9d2e5f658a0caf542714a26211bb4e53805941b
Jesus Fernandez <jesus@playhaven.com>
2011-04-25 18:14:34 -0700

    adds placeholder token and secret

---
+++ example/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//  playhaven-sdk-ios
+//
+//  Created by Jesus Fernandez on 4/25/11.
+//  Copyright 2011 Playhaven. All rights reserved.
+//
+
+#warning Don't forget to replace these values with your PlayHaven assigned token and secret.
+#define PH_TOKEN @"TOKEN"
+#define PH_SECRET @"SECRET"
