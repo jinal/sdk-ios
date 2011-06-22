88a08de822a4ca8877dcbc5d7f40aa06b31a379c
Jesus Fernandez <jesus@playhaven.com>
2011-06-22 11:34:58 -0700

    Created PHPublisherMetaDataRequest and associated tests

---
+++ src/PHPublisherMetadataRequest.h
@@ -0,0 +1,22 @@
+//
+//  PHPublisherMetadataRequest.h
+//  playhaven-sdk-ios
+//
+//  Created by Jesus Fernandez on 6/22/11.
+//  Copyright 2011 Playhaven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PHAPIRequest.h"
+
+@interface PHPublisherMetadataRequest : PHAPIRequest{
+  NSString *_placement;
+}
+
++(id)requestForApp:(NSString *)token secret:(NSString *)secret placement:(NSString *)placement delegate:(id)delegate;
+
+-(id)initWithApp:(NSString *)token secret:(NSString *)secret placement:(NSString *)placement delegate:(id)delegate;
+
+@property (nonatomic,copy) NSString *placement;
+
+@end
---
+++ tests/PHPublisherMetadataRequestTest.m
@@ -0,0 +1,24 @@
+//
+//  PHPublisherMetadataRequestTest.m
+//  playhaven-sdk-ios
+//
+//  Created by Jesus Fernandez on 3/30/11.
+//  Copyright 2011 Playhaven. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+#import "PHPublisherMetadataRequest.h"
+
+@interface PHPublisherMetadataRequestTest : SenTestCase
+@end
+
+
+@implementation PHPublisherMetadataRequestTest
+
+-(void)testInstance{
+  PHPublisherMetadataRequest *request = [PHPublisherMetadataRequest requestForApp:@"" secret:@"" placement:@"" delegate:self];
+  STAssertNotNil(request, @"expected request instance, got nil");
+}
+
+@end
