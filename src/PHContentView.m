14f877ec2bc26339de5c9c7ebbc93e60d269eb39
Jesus Fernandez <jesus@playhaven.com>
2011-11-18 15:35:30 -0800

    forgot to clear old PHContentView instances before recycling them

--- src/PHContentView.m
+++ src/PHContentView.m
@@ -75,6 +75,7 @@ static NSMutableSet *allContentViews = nil;
 +(void)enqueueContentViewInstance:(PHContentView *)contentView{
     //cleanup before enqueue
     contentView.delegate = nil;
+    [contentView.webView loadHTMLString:@"" baseURL:nil];
     
     [[self allContentViews] addObject:contentView];    
 }
